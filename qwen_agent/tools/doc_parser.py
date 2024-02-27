6b73b727ffe9a9c544e123671c00382c0b772964
兼欣 <jason.mjx@alibaba-inc.com>
2024-02-27 19:25:33 +0800

    bugfix: doc_parser with windows path

--- qwen_agent/tools/doc_parser.py
+++ qwen_agent/tools/doc_parser.py
@@ -77,7 +77,8 @@ def process_file(url: str, db: Storage = None):
     if url.split('.')[-1].lower() in ['pdf', 'docx', 'pptx']:
         date1 = datetime.datetime.now()
 
-        if url.startswith('https://') or url.startswith('http://'):
+        if url.startswith('https://') or url.startswith('http://') or re.match(
+                r'^[A-Za-z]:\\', url) or re.match(r'^[A-Za-z]:/', url):
             pdf_path = url
         else:
             parsed_url = urlparse(url)
