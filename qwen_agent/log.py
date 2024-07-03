6608d6be1f20836f43e554e68f68afc8140f9016
兼欣 <jason.mjx@alibaba-inc.com>
2024-07-03 14:53:40 +0800

    minor fix for logger.setLevel to function properly

--- qwen_agent/log.py
+++ qwen_agent/log.py
@@ -3,21 +3,21 @@ import os
 
 
 def setup_logger(level=None):
-
     if level is None:
-        if int(os.getenv('QWEN_AGENT_DEBUG', '0').strip()):
+        if os.getenv('QWEN_AGENT_DEBUG', '0').strip().lower() in ('1', 'true'):
             level = logging.DEBUG
         else:
             level = logging.INFO
 
-    logger = logging.getLogger('qwen_agent_logger')
-    logger.setLevel(level)
     handler = logging.StreamHandler()
-    handler.setLevel(level)
+    # Do not run handler.setLevel(level) so that users can change the level via logger.setLevel later
     formatter = logging.Formatter('%(asctime)s - %(filename)s - %(lineno)d - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+
+    _logger = logging.getLogger('qwen_agent_logger')
+    _logger.setLevel(level)
+    _logger.addHandler(handler)
+    return _logger
 
 
 logger = setup_logger()
