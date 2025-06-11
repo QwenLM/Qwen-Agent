616381a49c7d85f1d89c6c3c43e23cb5aa86148a
ethan <ethan.yang@intel.com>
2025-06-10 22:08:59 -0700

    remove stop token

--- qwen_agent/llm/openvino.py
+++ qwen_agent/llm/openvino.py
@@ -79,32 +79,6 @@ class OpenVINO(BaseFnCallModel):
         )
         self.tokenizer = AutoTokenizer.from_pretrained(cfg['ov_model_dir'])
 
-    def _get_stopping_criteria(self, generate_cfg: dict):
-        from transformers.generation.stopping_criteria import StoppingCriteria, StoppingCriteriaList
-
-        class StopSequenceCriteria(StoppingCriteria):
-            """
-            This class can be used to stop generation whenever a sequence of tokens is encountered.
-
-            Args:
-                stop_sequences (`str` or `List[str]`):
-                    The sequence (or list of sequences) on which to stop execution.
-                tokenizer:
-                    The tokenizer used to decode the model outputs.
-            """
-
-            def __init__(self, stop_sequences, tokenizer):
-                if isinstance(stop_sequences, str):
-                    stop_sequences = [stop_sequences]
-                self.stop_sequences = stop_sequences
-                self.tokenizer = tokenizer
-
-            def __call__(self, input_ids, scores, **kwargs) -> bool:
-                decoded_output = self.tokenizer.decode(input_ids.tolist()[0])
-                return any(decoded_output.endswith(stop_sequence) for stop_sequence in self.stop_sequences)
-
-        return StoppingCriteriaList([StopSequenceCriteria(generate_cfg['stop'], self.tokenizer)])
-
     def _chat_stream(
         self,
         messages: List[Message],
@@ -121,9 +95,7 @@ class OpenVINO(BaseFnCallModel):
                 input_ids=input_token,
                 streamer=streamer,
                 max_new_tokens=generate_cfg.get('max_new_tokens', 2048),
-                stopping_criteria=self._get_stopping_criteria(generate_cfg=generate_cfg),
             ))
-        del generate_cfg['stop']
         del generate_cfg['seed']
 
         def generate_and_signal_complete():
@@ -151,9 +123,7 @@ class OpenVINO(BaseFnCallModel):
             dict(
                 input_ids=input_token,
                 max_new_tokens=generate_cfg.get('max_new_tokens', 2048),
-                stopping_criteria=self._get_stopping_criteria(generate_cfg=generate_cfg),
             ))
-        del generate_cfg['stop']
         del generate_cfg['seed']
 
         response = self.ov_model.generate(**generate_cfg)
